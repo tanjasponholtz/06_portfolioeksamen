e8fe4db086809308bc1079b874d2b722db1a6dbf
tanjasponholtz <tanj8868@stud.kea.dk>
2022-01-09 23:35:34 +0100

    comit

---
+++ script.js
@@ -0,0 +1,20 @@
+// const btn = document.querySelector("button");
+// const ul = document.querySelector("nav");
+
+// function toggleMenu() {
+//   ul.classList.toggle("shown");
+
+//   const menu = ul.classList.contains("shown");
+
+//   if (menu) {
+//     // hvis ul har klassen "shown"
+//     //btn.textContent = "Luk";
+//     btn.classList.add("open");
+//   } else {
+//     // hvis IKKE ul har klassen "shown"
+//     //btn.textContent = "Menu";
+//     btn.classList.remove("open");
+//   }
+// }
+
+// btn.addEventListener("click", toggleMenu);
