3decfa66576e7e948598a97f8f003ae781663639
tanjasponholtz <tanj8868@stud.kea.dk>
2022-01-11 13:01:15 +0100

    comit

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-// const btn = document.querySelector("button");
-// const ul = document.querySelector("nav");
+// //lav en variabel der refererer til .toggle-btn
+// const btn = document.querySelector(".toggle-btn");
+// //lav en variabel der refererer til nav
+// const btn = document.querySelector("nav");
 
+// //skjul luk, så den kun er fremme når man trykker på burgermenu
+// document.querySelector("#luk").classList.add("hide");
+
+// //med de her to punkter kan man klikke linjerne frem
 // function toggleMenu() {
-//   ul.classList.toggle("shown");
+//   nav.classList.toggle("shown");
 
-//   const menu = ul.classList.contains("shown");
+//   //nav variabel med klassen shown
+//   const menuShown = nav.classList.contains("shown");
 
-//   if (menu) {
-//     // hvis ul har klassen "shown"
-//     //btn.textContent = "Luk";
-//     btn.classList.add("open");
+//   //if/else
+//   if (menuShown) {
+//     //hvis nav har klassen shown, så tilføjes luk
+//     luk();
 //   } else {
-//     // hvis IKKE ul har klassen "shown"
-//     //btn.textContent = "Menu";
-//     btn.classList.remove("open");
+//     //hvis nav ikke har klassen shown, så tilføjes menu
+//     menu();
 //   }
 // }
-
-// btn.addEventListener("click", toggleMenu);
+// function luk() {
+//   document.querySelector("#luk").classList.remove("hide");
+//   document.querySelector("#menu").classList.add("hide");
+// }
+// function menu() {
+//   document.querySelector("#menu").classList.remove("hide");
+//   document.querySelector("#luk").classList.add("hide");
+// }
+// //tilføj click event til btn, der sætter toggleMenu-funktionen igang
+// btn.addEventListener("click, toggleMenu");
+function myFunction(x) {
+  x.classList.toggle("change");
+}
